e5761589324ecf979fe713e91a3d8be58441639a
최 혜진(Ella) <ella@optolane.com>
2021-09-07 12:38:16 +0900

    menu 수정

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,13 +1,15 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="/">Home</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <!-- <b-nav-item href="#">Link</b-nav-item> -->
+        <b-nav-item href="/signup">Sign up</b-nav-item>
+        <b-nav-item href="/login">Login</b-nav-item>
+        <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
