f4bb92d33a5f165071798fbdcd70705f8f8996da
최혜진 <choehyejin@choehyejin-ui-MacBookAir.local>
2021-09-12 12:14:45 +0900

    몽지성 빡침

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <NavBar /> 
+    </div>
+    <b-container>
+      <router-view />
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import NavBar from "@/components/NavBar";
+  export default {
+    components: {
+      NavBar
+    }
+  }
+</script>
+<style lang="scss">
+</style>
